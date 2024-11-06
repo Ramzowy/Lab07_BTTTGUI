b9b0b6e51e64b135d59dcecafe6e522d7ea154df
Ramzowy <ramzowy@gmail.com>
2024-11-06 13:47:52 -0500

    Invoice

---
+++ Lab07A_Invoice/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Invoice invoice = new Invoice("Sam's Appliance Invoice","2407 Clifton Avenue","Cincinnati","OH",45219);
+    }
+}
